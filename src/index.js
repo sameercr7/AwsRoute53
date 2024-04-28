57fd2ceb6bd7250bd1b91e6f808183e2d124d7c1
sameerCr7 <sameer05072001@gmail.com>
2024-04-28 23:11:59 +0530

    Frontend Update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { createRoot } from 'react-dom/client';
 // Configurable Auth0 setup
 const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN || 'dev-nkqn1tvo4itiflhx.us.auth0.com';
 const auth0ClientId = process.env.REACT_APP_AUTH0_CLIENT_ID || 'Hv04ZZbJ2Ib2upQfAL85MPF5DrSSZj8D';
-const auth0secret=process.env.REACT_APP_AUTH0_Secret||'fzgzHSKObp9Q1fEOkoJHsz-Z2uiTnK2Fy-Kd2R7xX_Sv7a1Aj7si1eRAJ1wJRvLN'
 const auth0Audience = process.env.REACT_APP_AUTH0_AUDIENCE || 'https://dev-nkqn1tvo4itiflhx.us.auth0.com/api/v2/';
 
 createRoot(document.getElementById('root')).render(
@@ -17,6 +16,5 @@ createRoot(document.getElementById('root')).render(
     authorizationParams={{ redirect_uri: window.location.origin }}
   >
     <App />
-  </Auth0Provider>,
-  document.getElementById('root')
+  </Auth0Provider>
 );
